e36562a19c47f9c2b1e3cd955359ffede5431882
takuya nagasawa <tsukasayanagaw474@gmail.com>
2021-04-17 18:46:04 +0900

    重複処理の削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,11 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(post_params)
     if params[:back]
@@ -18,32 +20,37 @@ class PostsController < ApplicationController
       end
     end
   end
+
   def show
-    @post = Post.find(params[:id])
   end
+
   def edit
-    @post = Post.find(params[:id])
   end
+
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to posts_path, notice: "つぶやきを編集しました！"
     else
       render :edit
     end
   end
+
   def destroy
     @post.destroy
     redirect_to posts_path, notice:"つぶやきを削除しました！"
   end
+
   def confirm
     @post = Post.new(post_params)
     render :new if @post.invalid?
   end
+
   private
+  
   def post_params
     params.require(:post).permit(:content)
   end
+
   def set_post
     @post = Post.find(params[:id])
   end
